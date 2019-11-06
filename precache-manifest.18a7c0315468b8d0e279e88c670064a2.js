e9f41098dbe6a4d248d71de9826390fe91660f01
matthewmridha <45836769+matthewmridha@users.noreply.github.com>
2019-11-06 14:57:10 +0600

    Updates

---
+++ precache-manifest.18a7c0315468b8d0e279e88c670064a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cacf8e66db305005bf17747a6d40512",
+    "url": "/random-quote-machine/index.html"
+  },
+  {
+    "revision": "d6ae0c6805db923aa553",
+    "url": "/random-quote-machine/static/css/main.ffd594bb.chunk.css"
+  },
+  {
+    "revision": "5d58632323a46d62262c",
+    "url": "/random-quote-machine/static/js/2.e2eda111.chunk.js"
+  },
+  {
+    "revision": "d6ae0c6805db923aa553",
+    "url": "/random-quote-machine/static/js/main.206d58a1.chunk.js"
+  },
+  {
+    "revision": "a1217defecffe873141b",
+    "url": "/random-quote-machine/static/js/runtime-main.6d700caf.js"
+  }
+]);
